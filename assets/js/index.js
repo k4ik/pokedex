f2d206250f3b8cbc5d5a273963460e792ea275ab
k4ik <kaikleal1234@gmail.com>
2023-08-27 19:42:03 -0300

    feat: define colors and types of pokemons

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,27 @@
 const pokeContainer = document.querySelector('#pokeContainer');
+const pokemonsCount = 150;
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+}
+
+const mainTypes = Object.keys(colors);
 
 const getPokemons = async () => {
     const url = `https://pokeapi.co/api/v2/pokemon`
     const resp = await fetch(url)
     const data = await resp.json() 
-    console.log(data);
 }
 
-getPokemons();
